bb43ef6ab309c888f72c812d8cf2bbb02ad5da49
Irvine <quynhngoc4076@gmail.com>
2026-05-24 00:05:52 +0700

    feat: enhance HTML content of LearningPathPage

--- src/pages/LearningPathPage.tsx
+++ src/pages/LearningPathPage.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./LearningPathPage.css";
 
 export const LearningPathPage: React.FC = () => {
   return (
     <div className="learning-path-page">
-      <h1>Learning Path</h1>
-      <p>Your journey to frontend mastery</p>
+      <div className="learning-path-header">
+        <span id="verified">
+          <i>
+            <img
+              src="\learning-path\certificate_icon.svg"
+              alt="Verified "
+            ></img>
+          </i>
+          <i>CERTIFICATION PATH</i>
+        </span>
+        <div>Frontend Learning Path</div>
+        <div>
+          Master the art of building modern interfaces from core fundamentals to
+          advanced DOM manipulation and performance debugging.
+        </div>
+      </div>
     </div>
   );
 };
